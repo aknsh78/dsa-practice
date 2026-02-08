5c7478f49d203dd30bfb429964e12f0ae8a4ef02
aknsh78 <akanshakukshal@gmail.com>
2026-02-08 16:33:26 +0530

    update

--- dsa/lec3-a.cpp
+++ dsa/lec3-a.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main()
 {
+    //VECTOR
     pair<int,int> p1={1,3};
     cout<<p1.first<<"  "<<p1.second<<endl;
     //2nd
@@ -76,7 +77,8 @@ int main()
     v.clear();
 
     cout<<v.empty();//t or f
-    // list
+
+    // LIST
     void list()
     {
     list<lst> ls;
@@ -86,7 +88,7 @@ int main()
     ls.emplace_front(10);//insert at beg
     }
 
-    //dequeue
+    //DEQUE
     void explainDeque()
     {
         deque<int>dq;
@@ -97,7 +99,7 @@ int main()
         dq.back();
         dq.front();
     }
-    //stack
+    //STACK
     void explainStack()
     {
          stack<int>st;
@@ -113,7 +115,7 @@ int main()
          stack<int>st1,st2;
          st1.swap(st2);
     }
-    //queue
+    //QUEUE
     void eplainQueue()
     {
         queue<int>q;
@@ -126,6 +128,8 @@ int main()
         q.pop();//delete first element in queue
         cout<<q.front();
     }
+
+    //PRIORITY QUEUE
     void explainPQ()
     {
         priority_queue<int>pq;
@@ -147,6 +151,131 @@ int main()
         
 
     }
+    //SET
+    void explainSet()
+    {
+        set<int>st;
+        st.insert(1);
+        st.insert(2);
+        st.insert(2);//X
+        st.insert(4);
+        st.emplace(3);
+        //function
+        auto it=st.find(3);
+        auto it=st.find(6);//return st.end()
+        
+        int cnt=st.count(1);//if exist contain 1 occurence or else 0
+         
+        //erse differemt method
+         st.erase(2);//remove 2 and maintain the sorted order
+        
+        auto it=st.find(3);
+        st.erase(it);
+
+        auto it1=st.find(2);
+        auto it2=st.find(4);
+        st.erase(it1,it2);
+        //
+        auto it=st.lower_bound(2);
+        auto it=st.upper_bound(3);
+
+    }
+
+    void explainMUltiset()
+    {
+        multiset<int>ms;
+        ms.insert(1);
+        ms.insert(1);
+        ms.insert(1);//{1,1,1}
+        ms.erase(1);//delete all occurence of 1
+        int cnt=ms.count(1);
+        ms.erase(ms.find(1));//only a single one is erased
+        ms.erase(ms.find(1),ms.find(1)+2);
+
+    }
+    void explainUset()
+    {
+        unordered_set<int>st;
+    }
+    //MAP
+
+    void explainMap()
+    {
+        map<int,int>mpp;
+        mpp[1]=2;
+        mpp.emplace({3,4})
+        mpp.insert({2,4})
+        map<int,pair<int,int>>mpp;
+        map<pair<int,int>,int>mpp;
+        mpp[{2,3}]=10;
+        //traveersal
+        for(auto it:map)
+        {
+            cout<<it.first<<" "<<it.second<,endl;
+        }
+        cout<<mpp[1];
+        cout<<mpp[6];//if does not exists give null or 0
+        aauto it=mpp.find(3);
+        cout<<*(it).second;//value
+        auto it=mpp.find(5);//if does not exist points to end of the map
+        //
+        auto it=mpp.lower_bound(2);
+        auto it=mpp.upper_bound(3);
+        //erase,swap,size,empty are same
+
+
+
+    }
+    void explainMultimap()
+    {
+
+    }
+    void explainUnorderedMap()
+    {
+
+    }
+
+    //
+    bool comp(pair<int,int>p1,pair<int,int>p2)
+    {
+         if(p1.second < p2.second) return true;
+         if(p1.second > p2.second) return false;//swap
+         //they are same 
+         if(p1.first > p2.first) return true;
+         return false;//swap
+           
+    }
+
+    //***IMPRTANT***
+    void explainExtra()
+    {
+        sort(a,a+n);
+        sort(v.begin(),v.end());
+        sort(a+2,a+4);
+        sort(a,a+n,greater<int>);//descending order automatically
+        pair<int,int>a[]={{1,2},{3,4},{5,6}};
+        //myway
+        sort(a,a+n,comp);
+
+        //count 1 in binary representation of the number
+        int num=7;
+        int cnt=_builtin_popcount(num);
+        long long num=14852584568232265;
+        int cnt=_builtin_popcountll(num);
+        
+        //permuation 
+        string s="123";
+        sort(s.begin(),s.end());
+        do{
+            cout<<s<<endl;
+        }while(next_permutation(s.begin(),s.end()));
+
+        int maxi= *max_element(a,a+n);
+         
+
+    }
+     
+
 
 
 

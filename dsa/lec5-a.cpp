6b6afe5e9e0b401959aa4204e8ab88691e6480b7
aknsh78 <akanshakukshal@gmail.com>
2026-02-10 20:45:21 +0530

    update

--- dsa/lec5-a.cpp
+++ dsa/lec5-a.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 //recursion
 int count1=0;
+int sum(int n)
+{
+    if(n==0) return 0;
+    return n+sum(n-1);
+}
+int fact(int n)
+{
+    if (n==1) return 1;
+    return n*fact(n-1); 
+
+}
 void print()
 {
     
-    if (count1==3)
+    if (count1==3)  
     {
         return;
     }
@@ -15,8 +26,13 @@ void print()
 
     //cout<<1<<endl;
     //print();//infite loop
+    
 }
 int main()
 {
-    print();
-}0 
+    /*print();*/
+    int n=4;
+    cout<<sum(n);
+    cout<<" \nfactorial of a number is :"<<fact(n);
+
+}
